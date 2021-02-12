625e5b9b146cf0a53bab1cdfeb6759848e8718af
김진욱 <jusjinuk@gmail.com>
2021-02-13 03:45:32 +0900

    Add retrofit UserService.

--- app/src/main/java/com/wafflestudio/snuboard/data/retrofit/service/UserService.kt
+++ app/src/main/java/com/wafflestudio/snuboard/data/retrofit/service/UserService.kt
@@ -1,16 +1,55 @@
 package com.wafflestudio.snuboard.data.retrofit.service
 
+import com.wafflestudio.snuboard.data.retrofit.dto.PatchUserDto
+import com.wafflestudio.snuboard.data.retrofit.dto.UserDto
 import com.wafflestudio.snuboard.data.retrofit.dto.UserTokenDto
+import io.reactivex.rxjava3.core.Single
 import retrofit2.Call
-import retrofit2.http.Field
-import retrofit2.http.FormUrlEncoded
-import retrofit2.http.GET
+import retrofit2.Response
+import retrofit2.http.*
 
 interface UserService {
+
     @FormUrlEncoded
-    @GET("/users/auth/token/")
-    fun refreshWithToken(
+    @POST("users/")
+    fun postUser(
+        @Field("username") username: String,
+        @Field("password") password: String,
+        @Field("nickname") nickname: String
+    ): Single<Response<UserTokenDto>>
+
+    @FormUrlEncoded
+    @POST("users/auth/token/")
+    fun authWithPassword(
+        @Field("grant_type") grantType: String,
+        @Field("username") username: String,
+        @Field("password") password: String
+    ): Single<Response<UserTokenDto>>
+
+    @FormUrlEncoded
+    @POST("users/auth/token/")
+    fun authWithToken(
         @Field("grant_type") grantType: String,
         @Field("refresh_token") refreshToken: String
     ): Call<UserTokenDto>
+
+    @GET("users/me/")
+    fun getUserMe(): Single<Response<UserDto>>
+
+    @PATCH("users/me/")
+    fun patchUserMe(
+        @Body body: PatchUserDto
+    ): Single<Response<UserDto>>
+
+    @FormUrlEncoded
+    @DELETE("users/me/keyword/")
+    fun deleteUserKeyword(
+        @Field("keyword") keyword: String,
+    ): Single<Response<UserDto>>
+
+    @FormUrlEncoded
+    @POST("users/me/keyword/")
+    fun postUserKeyword(
+        @Field("keyword") keyword: String,
+    ): Single<Response<UserDto>>
 }
