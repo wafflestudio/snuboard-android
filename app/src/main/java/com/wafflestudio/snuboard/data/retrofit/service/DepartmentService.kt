d2580615172e63726f050ccd7ea6043f0410a46c
김진욱 <jusjinuk@gmail.com>
2021-02-13 17:05:01 +0900

    Add retrofit NoticeService except for search apis.

--- app/src/main/java/com/wafflestudio/snuboard/data/retrofit/service/DepartmentService.kt
+++ app/src/main/java/com/wafflestudio/snuboard/data/retrofit/service/DepartmentService.kt
@@ -2,28 +2,29 @@ package com.wafflestudio.snuboard.data.retrofit.service
 
 import com.wafflestudio.snuboard.data.retrofit.dto.DepartmentDto
 import io.reactivex.rxjava3.core.Single
+import retrofit2.Response
 import retrofit2.http.*
 
 interface DepartmentService {
     @GET("departments/")
-    fun getDepartments(): Single<List<DepartmentDto>>
+    fun getDepartments(): Single<Response<List<DepartmentDto>>>
 
     @GET("departments/{department_id}/")
     fun getDepartmentById(
             @Path("department_id") departmentId: Int
-    ): Single<DepartmentDto>
+    ): Single<Response<DepartmentDto>>
 
     @FormUrlEncoded
     @DELETE("departments/{department_id}/follow/")
     fun deleteFollowOfDepartment(
             @Path("department_id") departmentId: Int,
             @Field("follow") follow: String
-    ): Single<DepartmentDto>
+    ): Single<Response<DepartmentDto>>
 
     @FormUrlEncoded
     @POST("departments/{department_id}/follow/")
     fun postFollowOfDepartment(
             @Path("department_id") departmentId: Int,
             @Field("follow") follow: String
-    ): Single<DepartmentDto>
+    ): Single<Response<DepartmentDto>>
 }
