0950aa4db719e24e249d7631b2dc269bc62b51eb
김진욱 <jusjinuk@gmail.com>
2021-02-14 01:36:53 +0900

    Add data bindings to views.

--- app/src/main/java/com/wafflestudio/snuboard/presentation/department/DepartmentSearchActivity.kt
+++ app/src/main/java/com/wafflestudio/snuboard/presentation/department/DepartmentSearchActivity.kt
@@ -2,11 +2,23 @@ package com.wafflestudio.snuboard.presentation.department
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.wafflestudio.snuboard.R
+import com.wafflestudio.snuboard.databinding.ActivityDepartmentSearchBinding
 
 class DepartmentSearchActivity : AppCompatActivity() {
+
+    private val binding: ActivityDepartmentSearchBinding by lazy {
+        DataBindingUtil.setContentView(
+            this,
+            R.layout.activity_department_search
+        ) as ActivityDepartmentSearchBinding
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_department_search)
+        binding.run {
+
+        }
     }
 }
--- app/src/main/java/com/wafflestudio/snuboard/presentation/notice/NoticeSearchActivity.kt
+++ app/src/main/java/com/wafflestudio/snuboard/presentation/notice/NoticeSearchActivity.kt
@@ -2,11 +2,23 @@ package com.wafflestudio.snuboard.presentation.notice
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.wafflestudio.snuboard.R
+import com.wafflestudio.snuboard.databinding.ActivityNoticeSearchBinding
 
 class NoticeSearchActivity : AppCompatActivity() {
+
+    private val binding: ActivityNoticeSearchBinding by lazy {
+        DataBindingUtil.setContentView(
+            this,
+            R.layout.activity_notice_search
+        ) as ActivityNoticeSearchBinding
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_notice_search)
+        binding.run {
+           
+        }
     }
 }
