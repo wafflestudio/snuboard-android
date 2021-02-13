63b87dbfe32e079b4ae232c07a22e3f70e75cd01
김진욱 <jusjinuk@gmail.com>
2021-02-14 02:22:03 +0900

    Reflect snake_case related changes in response.

--- app/src/main/java/com/wafflestudio/snuboard/data/retrofit/dto/NoticeModel.kt
+++ app/src/main/java/com/wafflestudio/snuboard/data/retrofit/dto/NoticeModel.kt
@@ -1,22 +1,31 @@
 package com.wafflestudio.snuboard.data.retrofit.dto
 
+import com.google.gson.annotations.SerializedName
+
 data class NoticeList(
         val notices: List<Notice>,
+        @SerializedName("next_cursor")
         val nextCursor: String
 )
 
 data class Notice(
         val id: Int,
+        @SerializedName("department_name")
         val departmentName: String,
+        @SerializedName("department_id")
         val departmentId: Int,
         val title: String,
         val preview: String,
         val content: String,
+        @SerializedName("created_at")
         val createdAt: String,
         val tags: List<String>,
+        @SerializedName("is_pinned")
         val isPinned: Boolean,
         val link: String,
-        val files: List<NoticeFile>
+        val files: List<NoticeFile>,
+        @SerializedName("is_scrapped")
+        val isScrapped: Boolean
 )
 
 data class NoticeFile(
