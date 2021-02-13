be340d2b3664cd6f84023f6d0eb0472b8febd072
김진욱 <jusjinuk@gmail.com>
2021-02-14 00:56:04 +0900

    Add basic view skeleton.

--- app/src/main/java/com/wafflestudio/snuboard/presentation/notice/NoticeDetailActivity.kt
+++ app/src/main/java/com/wafflestudio/snuboard/presentation/notice/NoticeDetailActivity.kt
@@ -4,9 +4,9 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.wafflestudio.snuboard.R
 
-class MainActivity : AppCompatActivity() {
+class NoticeDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_notice_detail)
     }
 }
---
+++ app/src/main/java/com/wafflestudio/snuboard/presentation/notice/NoticeSearchActivity.kt
@@ -0,0 +1,12 @@
+package com.wafflestudio.snuboard.presentation.notice
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.wafflestudio.snuboard.R
+
+class NoticeSearchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_notice_search)
+    }
+}
