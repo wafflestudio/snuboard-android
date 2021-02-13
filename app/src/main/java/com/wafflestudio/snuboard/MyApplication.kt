241c9675560825c80385204f82821c1790b2f583
김진욱 <jusjinuk@gmail.com>
2021-02-14 02:24:05 +0900

    Plant Timber.

--- app/src/main/java/com/wafflestudio/snuboard/MyApplication.kt
+++ app/src/main/java/com/wafflestudio/snuboard/MyApplication.kt
@@ -2,8 +2,13 @@ package com.wafflestudio.snuboard
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
 class MyApplication : Application() {
-
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG)
+            Timber.plant(Timber.DebugTree())
+    }
 }
