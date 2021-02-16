c49433cd43d8fef98f71d66924008b298100ae80
김진욱 <jusjinuk@gmail.com>
2021-02-16 19:56:43 +0900

    Add toolbars to main fragments.

--- app/src/main/java/com/wafflestudio/snuboard/presentation/department/DepartmentFollowListFragment.kt
+++ app/src/main/java/com/wafflestudio/snuboard/presentation/department/DepartmentFollowListFragment.kt
@@ -6,24 +6,35 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import com.wafflestudio.snuboard.R
 import com.wafflestudio.snuboard.databinding.FragmentDepartmentFollowListBinding
+import com.wafflestudio.snuboard.presentation.MainActivityViewModel
 
 class DepartmentFollowListFragment : Fragment() {
 
     lateinit var binding: FragmentDepartmentFollowListBinding
+    private val mainActivityViewModel: MainActivityViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding =
-            DataBindingUtil.inflate(
-                inflater,
-                R.layout.fragment_department_follow_list,
-                container,
-                false
-            )
+                DataBindingUtil.inflate(
+                        inflater,
+                        R.layout.fragment_department_follow_list,
+                        container,
+                        false
+                )
+        binding.run {
+            toolBar.also { tb ->
+                tb.setNavigationIcon(R.drawable.ic_menu)
+                tb.setNavigationOnClickListener {
+                    mainActivityViewModel.setDrawer(true)
+                }
+            }
+        }
         return binding.root
     }
 }
