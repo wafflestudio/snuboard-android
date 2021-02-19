55c5b0f3de1730ab1db0cbfa049d39bb2db22528
김진욱 <jusjinuk@gmail.com>
2021-02-19 20:15:36 +0900

    Add AuthActivity with login/signup.

---
+++ app/src/main/java/com/wafflestudio/snuboard/presentation/auth/AuthActivity.kt
@@ -0,0 +1,25 @@
+package com.wafflestudio.snuboard.presentation.auth
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.wafflestudio.snuboard.R
+import com.wafflestudio.snuboard.databinding.ActivityAuthBinding
+
+class AuthActivity : AppCompatActivity() {
+
+
+    private val binding: ActivityAuthBinding by lazy {
+        DataBindingUtil.setContentView(
+                this,
+                R.layout.activity_auth
+        ) as ActivityAuthBinding
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding.run {
+
+        }
+    }
+}
