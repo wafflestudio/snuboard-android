bdb03f4e1402f2136b9da95c9cf56e38b29ad42b
김진욱 <jusjinuk@gmail.com>
2021-03-01 11:19:25 +0900

    Add app bar and tab layout to DepartmentActivity.

--- app/src/main/java/com/wafflestudio/snuboard/presentation/department/DepartmentFeedFragment.kt
+++ app/src/main/java/com/wafflestudio/snuboard/presentation/department/DepartmentFeedFragment.kt
@@ -1,29 +1,36 @@
 package com.wafflestudio.snuboard.presentation.department
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.wafflestudio.snuboard.R
 import com.wafflestudio.snuboard.databinding.FragmentDepartmentFeedBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class DepartmentFeedFragment : Fragment() {
 
     lateinit var binding: FragmentDepartmentFeedBinding
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View {
         binding =
-            DataBindingUtil.inflate(
-                inflater,
-                R.layout.fragment_department_feed,
-                container,
-                false
-            )
+                DataBindingUtil.inflate(
+                        inflater,
+                        R.layout.fragment_department_feed,
+                        container,
+                        false
+                )
+        setHasOptionsMenu(true)
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        menu.clear()
+        inflater.inflate(R.menu.app_bar_fragment_department_feed, menu)
     }
 }
--- app/src/main/java/com/wafflestudio/snuboard/presentation/department/DepartmentHomeFragment.kt
+++ app/src/main/java/com/wafflestudio/snuboard/presentation/department/DepartmentHomeFragment.kt
@@ -1,29 +1,36 @@
 package com.wafflestudio.snuboard.presentation.department
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.wafflestudio.snuboard.R
 import com.wafflestudio.snuboard.databinding.FragmentDepartmentHomeBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class DepartmentHomeFragment : Fragment() {
 
     lateinit var binding: FragmentDepartmentHomeBinding
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View {
         binding =
-            DataBindingUtil.inflate(
-                inflater,
-                R.layout.fragment_department_home,
-                container,
-                false
-            )
+                DataBindingUtil.inflate(
+                        inflater,
+                        R.layout.fragment_department_home,
+                        container,
+                        false
+                )
+        setHasOptionsMenu(true)
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        menu.clear()
+        inflater.inflate(R.menu.app_bar_fragment_department_home, menu)
     }
 }
