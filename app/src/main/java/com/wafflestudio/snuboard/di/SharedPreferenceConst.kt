b08071310501e9767ed425cd2d6f7931063d095b
김진욱 <jusjinuk@gmail.com>
2021-03-03 00:58:51 +0900

    Implement color logic with SharedPreference.

--- app/src/main/java/com/wafflestudio/snuboard/di/SharedPreferenceConst.kt
+++ app/src/main/java/com/wafflestudio/snuboard/di/SharedPreferenceConst.kt
@@ -3,4 +3,8 @@ package com.wafflestudio.snuboard.di
 object SharedPreferenceConst {
     const val ACCESS_TOKEN_KEY = "access_token_key"
     const val REFRESH_TOKEN_KEY = "refresh_token_key"
+
+    private const val DEPARTMENT_KEY_PREFIX = "department_key_"
+    fun getDepartmentKey(departmentId: Int): String =
+            DEPARTMENT_KEY_PREFIX + departmentId.toString()
 }
--- app/src/main/java/com/wafflestudio/snuboard/domain/translater/DepartmentMapper.kt
+++ app/src/main/java/com/wafflestudio/snuboard/domain/translater/DepartmentMapper.kt
@@ -1,23 +1,48 @@
 package com.wafflestudio.snuboard.domain.translater
 
+import android.content.Context
+import androidx.core.content.edit
+import androidx.preference.PreferenceManager
 import com.wafflestudio.snuboard.data.retrofit.dto.DepartmentDto
+import com.wafflestudio.snuboard.di.SharedPreferenceConst
 import com.wafflestudio.snuboard.domain.model.Department
 import com.wafflestudio.snuboard.domain.model.DepartmentColor
+import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class DepartmentMapper
 @Inject
-constructor() {
+constructor(@ApplicationContext appContext: Context) {
+
+    private val pref = PreferenceManager.getDefaultSharedPreferences(
+            appContext
+    )
 
     fun mapFromListDepartmentDto(dto: List<DepartmentDto>): List<Department> {
         return dto.map {
-            Department(it.id, it.name, it.tags, it.follow, DepartmentColor.SKY)
+            val preferenceKey = SharedPreferenceConst.getDepartmentKey(it.id)
+            var colorId = pref.getInt(preferenceKey, -1)
+            if (colorId == -1) {
+                pref.edit {
+                    putInt(preferenceKey, DepartmentColor.KOREAN_DAISY.colorId)
+                }
+                colorId = DepartmentColor.KOREAN_DAISY.colorId
+            }
+            Department(it.id, it.name, it.tags, it.follow, DepartmentColor.fromColorId(colorId)!!)
         }
     }
 
     fun mapFromDepartmentDto(dto: DepartmentDto): Department {
-        return Department(dto.id, dto.name, dto.tags, dto.follow, DepartmentColor.SKY)
+        val preferenceKey = SharedPreferenceConst.getDepartmentKey(dto.id)
+        var colorId = pref.getInt(preferenceKey, -1)
+        if (colorId == -1) {
+            pref.edit {
+                putInt(preferenceKey, DepartmentColor.KOREAN_DAISY.colorId)
+            }
+            colorId = DepartmentColor.KOREAN_DAISY.colorId
+        }
+        return Department(dto.id, dto.name, dto.tags, dto.follow, DepartmentColor.fromColorId(colorId)!!)
     }
 }
