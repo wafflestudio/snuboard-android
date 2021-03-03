3715ba8677c0230af6370a1e26d8a0c2b206ec17
김진욱 <jusjinuk@gmail.com>
2021-03-03 23:04:27 +0900

    Add color logic.

--- app/src/main/java/com/wafflestudio/snuboard/presentation/department/DepartmentFeedFragment.kt
+++ app/src/main/java/com/wafflestudio/snuboard/presentation/department/DepartmentFeedFragment.kt
@@ -5,12 +5,14 @@ import android.view.*
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.preference.PreferenceManager
 import com.google.android.flexbox.FlexDirection
 import com.google.android.flexbox.FlexWrap
 import com.google.android.flexbox.FlexboxLayoutManager
 import com.google.android.flexbox.JustifyContent
 import com.wafflestudio.snuboard.R
 import com.wafflestudio.snuboard.databinding.FragmentDepartmentFeedBinding
+import com.wafflestudio.snuboard.domain.model.DepartmentColor
 import com.wafflestudio.snuboard.presentation.TagClickListener
 import com.wafflestudio.snuboard.presentation.TagListAdapter
 import dagger.hilt.android.AndroidEntryPoint
@@ -20,6 +22,11 @@ class DepartmentFeedFragment : Fragment() {
 
     lateinit var binding: FragmentDepartmentFeedBinding
     private val departmentActivityViewModel: DepartmentActivityViewModel by activityViewModels()
+    private val pref by lazy {
+        PreferenceManager.getDefaultSharedPreferences(
+                activity
+        )
+    }
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
@@ -35,6 +42,9 @@ class DepartmentFeedFragment : Fragment() {
         binding.run {
             lifecycleOwner = this@DepartmentFeedFragment
             activityViewModel = departmentActivityViewModel
+            colorClickListener = ColorClickListener { departmentColor ->
+                departmentActivityViewModel.changeDepartmentColor(departmentColor, pref)
+            }
             tagRecyclerView.run {
                 adapter = TagListAdapter(
                         TagClickListener {
@@ -57,4 +67,11 @@ class DepartmentFeedFragment : Fragment() {
         menu.clear()
         inflater.inflate(R.menu.app_bar_fragment_department_feed, menu)
     }
+}
+
+class ColorClickListener(
+        val clickListener: (departmentColor: DepartmentColor) -> Unit
+) {
+    fun onClick(departmentColor: DepartmentColor) =
+            clickListener(departmentColor)
 }
