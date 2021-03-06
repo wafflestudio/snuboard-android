4e715bbb8f19160192b8944a0f64567038da6015
김진욱 <jusjinuk@gmail.com>
2021-03-06 14:27:43 +0900

    Fix IME_ACTION_DONE action.

--- app/src/main/java/com/wafflestudio/snuboard/presentation/auth/SignUpFragment.kt
+++ app/src/main/java/com/wafflestudio/snuboard/presentation/auth/SignUpFragment.kt
@@ -1,11 +1,13 @@
 package com.wafflestudio.snuboard.presentation.auth
 
+import android.content.Context
 import android.os.Bundle
 import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
 import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
@@ -40,8 +42,11 @@ class SignUpFragment : Fragment() {
             }
             nicknameEditText.setOnEditorActionListener(object : TextView.OnEditorActionListener {
                 override fun onEditorAction(v: TextView?, actionId: Int, event: KeyEvent?): Boolean {
-                    if (actionId == EditorInfo.IME_ACTION_DONE) {
-                        signUpButton.performClick()
+                    if (actionId == EditorInfo.IME_ACTION_DONE
+                            || (event != null && (event.keyCode == KeyEvent.KEYCODE_ENTER))) {
+                        val imm = requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE)
+                                as InputMethodManager
+                        imm.hideSoftInputFromWindow(v!!.windowToken, 0)
                         return true
                     }
                     return false
