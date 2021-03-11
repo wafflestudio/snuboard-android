421a639f9e80fe6cb2cb6d29a15616404ea06c0b
김진욱 <jusjinuk@gmail.com>
2021-03-12 01:05:07 +0900

    Add titles, tags and contents in Notice Detail.

--- app/src/main/java/com/wafflestudio/snuboard/presentation/notice/NoticeDetailActivity.kt
+++ app/src/main/java/com/wafflestudio/snuboard/presentation/notice/NoticeDetailActivity.kt
@@ -4,17 +4,27 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.content.res.ColorStateList
+import android.net.Uri
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.core.content.res.ResourcesCompat
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.flexbox.FlexDirection
+import com.google.android.flexbox.FlexWrap
+import com.google.android.flexbox.FlexboxLayoutManager
+import com.google.android.flexbox.JustifyContent
 import com.wafflestudio.snuboard.R
 import com.wafflestudio.snuboard.databinding.ActivityNoticeDetailBinding
+import com.wafflestudio.snuboard.presentation.TagListAdapter
 import com.wafflestudio.snuboard.utils.SingleEvent
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -51,6 +61,36 @@ class NoticeDetailActivity : AppCompatActivity() {
                 setDisplayHomeAsUpEnabled(true)
                 setHomeButtonEnabled(true)
                 setHomeAsUpIndicator(R.drawable.ic_navigate_before)
+            }
+
+            val tagRecyclerView = binding.root.findViewById<RecyclerView>(R.id.tag_recycler_view)
+            tagRecyclerView.run {
+                adapter = TagListAdapter(null)
+                layoutManager = FlexboxLayoutManager(binding.root.context).apply {
+                    flexWrap = FlexWrap.WRAP
+                    flexDirection = FlexDirection.ROW
+                    justifyContent = JustifyContent.FLEX_START
+                }
+            }
+
+            contentWebView.apply {
+                webViewClient = object : WebViewClient() {
+                    override fun onPageFinished(view: WebView?, url: String?) {
+                        super.onPageFinished(view, url)
+                        mainLayout.requestLayout()
+                    }
+
+                    override fun shouldOverrideUrlLoading(
+                        view: WebView?,
+                        request: WebResourceRequest?
+                    ): Boolean {
+                        val url = request?.url ?: return false
+                        startActivity(Intent(Intent.ACTION_VIEW, url))
+                        return true
+                    }
+                }
+                settings.javaScriptEnabled = true
+                isEnabled = false
             }
         }
 
@@ -89,6 +129,13 @@ class NoticeDetailActivity : AppCompatActivity() {
         when (item.itemId) {
             R.id.scrap_button ->
                 noticeDetailActivityViewModel.toggleSavedNotice()
+            R.id.link_button -> {
+                val url = noticeDetailActivityViewModel.notice.value?.link
+                url?.let {
+                    startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(it)))
+                }
+                return true
+            }
             else ->
                 return false
         }
