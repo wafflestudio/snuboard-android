7fd65180cabe8c44c04ed338b94a194d93243d05
김진욱 <jusjinuk@gmail.com>
2021-03-12 21:44:37 +0900

    Improve minor UIs.

--- app/src/main/java/com/wafflestudio/snuboard/utils/BindingUtils.kt
+++ app/src/main/java/com/wafflestudio/snuboard/utils/BindingUtils.kt
@@ -165,7 +165,18 @@ fun bindLoadContent(view: WebView, content: String?) {
     content?.let {
         val parsedHTML =
                 """
-            <head><style type="text/css"> html, body { width:100%; height: 100%; margin: 0px; padding: 0px; } </style></head><body>
+            <head>
+            <style type="text/css"> 
+            html, body { 
+                font-size: 14px;
+                width:100%; 
+                height: 100%; 
+                margin: 0px; 
+                padding: 0px; 
+                } 
+            </style>
+            </head>
+            <body>
             """ + it + "</body>"
         view.loadData(parsedHTML, "text/html; charset=utf-8", "UTF-8")
     }
