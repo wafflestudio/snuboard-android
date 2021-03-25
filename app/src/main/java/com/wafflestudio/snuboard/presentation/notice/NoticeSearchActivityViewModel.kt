444cc0f3ef9781f85a9f99e803fa4975c8fc591c
김진욱 <jusjinuk@gmail.com>
2021-03-26 04:14:23 +0900

    Add basic search logic.

--- app/src/main/java/com/wafflestudio/snuboard/presentation/notice/NoticeSearchActivityViewModel.kt
+++ app/src/main/java/com/wafflestudio/snuboard/presentation/notice/NoticeSearchActivityViewModel.kt
@@ -1,14 +1,121 @@
 package com.wafflestudio.snuboard.presentation.notice
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import com.wafflestudio.snuboard.domain.model.Notice
+import com.wafflestudio.snuboard.domain.model.NoticeList
+import com.wafflestudio.snuboard.domain.usecase.DeleteNoticeScrapUseCase
+import com.wafflestudio.snuboard.domain.usecase.GetNoticeOfFollowSearchUseCase
+import com.wafflestudio.snuboard.domain.usecase.GetNoticesByFollowUseCase
+import com.wafflestudio.snuboard.domain.usecase.PostNoticeScrapUseCase
+import com.wafflestudio.snuboard.utils.ErrorResponse
+import com.wafflestudio.snuboard.utils.Event
+import com.wafflestudio.snuboard.utils.SingleEvent
 import dagger.hilt.android.lifecycle.HiltViewModel
+import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
 class NoticeSearchActivityViewModel
 @Inject
 constructor(
+        private val getNoticesByFollowUseCase: GetNoticesByFollowUseCase,
+        private val getNoticesOfFollowSearchUseCase: GetNoticeOfFollowSearchUseCase,
+        private val deleteNoticeScrapUseCase: DeleteNoticeScrapUseCase,
+        private val postNoticeScrapUseCase: PostNoticeScrapUseCase,
         private val savedStateHandle: SavedStateHandle
 ) : ViewModel() {
+
+    private val _notices = MutableLiveData<List<Notice>>()
+    val keywords = MutableLiveData<String>()
+    val updateNotice = getNoticesOfFollowSearchUseCase.updateNotice
+
+    val notices: LiveData<List<Notice>>
+        get() = _notices
+
+    private val paginationLimit = 10
+    private var paginationCursor: String? = null
+
+    fun cleanCursor() {
+        paginationCursor = null
+    }
+
+    fun getNotices() {
+        val tmpNoticeList = _notices.value?.toMutableList() ?: mutableListOf()
+        if (paginationCursor == EOP)
+            return
+        keywords.value?.let { keyword_string ->
+            getNoticesOfFollowSearchUseCase
+                    .getNotices(keyword_string, paginationLimit, paginationCursor)
+                    .subscribe({
+                        when (it) {
+                            is NoticeList -> {
+                                tmpNoticeList.addAll(it.notices)
+                                _notices.value = tmpNoticeList
+                                paginationCursor = if (it.nextCursor.isEmpty())
+                                    EOP
+                                else
+                                    it.nextCursor
+                            }
+                            is ErrorResponse -> {
+                                SingleEvent.triggerToast.value = Event(it.message)
+                                Timber.e(it.message)
+                            }
+                        }
+                    }, {
+                        Timber.e(it)
+                    })
+        }
+    }
+
+    fun updateNotice(notice: Notice) {
+        val tmpNoticeList = _notices.value?.toMutableList() ?: mutableListOf()
+        _notices.value = tmpNoticeList.map {
+            if (it.id == notice.id)
+                notice
+            else
+                it
+        }
+    }
+
+    fun toggleSavedNotice(noticeId: Int) {
+        val tmpNoticeList = _notices.value?.toMutableList() ?: mutableListOf()
+        tmpNoticeList.find {
+            it.id == noticeId
+        }?.let {
+            if (it.isScrapped)
+                deleteNoticeScrapUseCase
+                        .deleteNoticeScrapSimple(noticeId)
+            else
+                postNoticeScrapUseCase
+                        .postNoticeScrapSimple(noticeId)
+        }
+                ?.subscribe({
+                    when (it) {
+                        is Notice -> {
+                            _notices.value = tmpNoticeList.map { it1 ->
+                                if (it1.id == noticeId)
+                                    it
+                                else
+                                    it1
+                            }
+                            getNoticesByFollowUseCase.updateNotice(it)
+                        }
+                        is ErrorResponse -> {
+                            SingleEvent.triggerToast.value = Event(it.message)
+                            Timber.e(it.message)
+                        }
+                    }
+                }, {
+                    Timber.e(it)
+                })
+    }
+
+    companion object {
+        // Used to indicate cursor that it is End of Page
+        private const val EOP = "EOP"
+
+    }
 }
