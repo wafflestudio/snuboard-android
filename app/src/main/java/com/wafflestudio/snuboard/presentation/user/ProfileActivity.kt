e6cf4979d2a0f5e55825144fb7173a99292a75a0
김진욱 <jusjinuk@gmail.com>
2021-05-21 21:04:31 +0900

    Add ProfileActivity skeleton.

--- app/src/main/java/com/wafflestudio/snuboard/presentation/user/ProfileActivity.kt
+++ app/src/main/java/com/wafflestudio/snuboard/presentation/user/ProfileActivity.kt
@@ -3,25 +3,51 @@ package com.wafflestudio.snuboard.presentation.user
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.wafflestudio.snuboard.R
 import com.wafflestudio.snuboard.databinding.ActivityProfileBinding
+import com.wafflestudio.snuboard.utils.SingleEvent
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class ProfileActivity : AppCompatActivity() {
 
     private val binding: ActivityProfileBinding by lazy {
         DataBindingUtil.setContentView(
-                this,
-                R.layout.activity_profile
+            this,
+            R.layout.activity_profile
         ) as ActivityProfileBinding
     }
 
+    private val profileActivityViewModel: ProfileActivityViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding.run {
-
+        SingleEvent.triggerToast.observe(this) {
+            it.getContentIfNotHandled()?.let { it1 ->
+                Toast.makeText(this, it1, Toast.LENGTH_SHORT).show()
+            }
         }
+        binding.run {
+            lifecycleOwner = this@ProfileActivity
+            viewModel = profileActivityViewModel
+
+            setSupportActionBar(toolBar)
+            supportActionBar!!.apply {
+                setDisplayShowTitleEnabled(false)
+                setDisplayHomeAsUpEnabled(true)
+                setHomeButtonEnabled(true)
+                setHomeAsUpIndicator(R.drawable.ic_navigate_before)
+            }
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        finish()
+        return super.onSupportNavigateUp()
     }
 
     override fun finish() {
