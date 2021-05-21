929f80bf1268309f0e8a401b8dc08248a8846cf7
김진욱 <jusjinuk@gmail.com>
2021-05-21 20:19:21 +0900

    Add randomize logic in default department color.

--- app/src/main/java/com/wafflestudio/snuboard/domain/translater/DepartmentMapper.kt
+++ app/src/main/java/com/wafflestudio/snuboard/domain/translater/DepartmentMapper.kt
@@ -10,6 +10,7 @@ import com.wafflestudio.snuboard.domain.model.DepartmentColor
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 import javax.inject.Singleton
+import kotlin.random.Random
 
 @Singleton
 class DepartmentMapper
@@ -30,10 +31,32 @@ constructor(@ApplicationContext appContext: Context) {
         val preferenceKey = SharedPreferenceConst.getDepartmentColorKey(dto.id)
         var colorId = pref.getInt(preferenceKey, -1)
         if (colorId == -1) {
+            val r = Random.nextInt(0, 9)
             pref.edit {
-                putInt(preferenceKey, DepartmentColor.POMEGRANATE.colorId)
+                when (r) {
+                    0 -> putInt(preferenceKey, DepartmentColor.SKY.colorId)
+                    1 -> putInt(preferenceKey, DepartmentColor.POMEGRANATE.colorId)
+                    2 -> putInt(preferenceKey, DepartmentColor.CITRUS.colorId)
+                    3 -> putInt(preferenceKey, DepartmentColor.JADE.colorId)
+                    4 -> putInt(preferenceKey, DepartmentColor.KOREAN_DAISY.colorId)
+                    5 -> putInt(preferenceKey, DepartmentColor.PEA.colorId)
+                    6 -> putInt(preferenceKey, DepartmentColor.MEDITERRANEAN.colorId)
+                    7 -> putInt(preferenceKey, DepartmentColor.AMETHYST.colorId)
+                    8 -> putInt(preferenceKey, DepartmentColor.LAVENDER.colorId)
+                }
             }
-            colorId = DepartmentColor.POMEGRANATE.colorId
+            colorId = when (r) {
+                0 -> DepartmentColor.SKY.colorId
+                1 -> DepartmentColor.POMEGRANATE.colorId
+                2 -> DepartmentColor.CITRUS.colorId
+                3 -> DepartmentColor.JADE.colorId
+                4 -> DepartmentColor.KOREAN_DAISY.colorId
+                5 -> DepartmentColor.PEA.colorId
+                6 -> DepartmentColor.MEDITERRANEAN.colorId
+                7 -> DepartmentColor.AMETHYST.colorId
+                8 -> DepartmentColor.LAVENDER.colorId
+                else -> throw error("Not right random number.")
+            }
         }
         return Department(dto.id, dto.name, dto.college, dto.tags, dto.follow, DepartmentColor.fromColorId(colorId)!!)
     }
